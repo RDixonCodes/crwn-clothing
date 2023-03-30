db4de4adc9aa69aeec07373239f73d62e92a8ede
RDixonCodes <rhdixon79@gmail.com>
2023-03-29 21:52:55 -0700

    added sign in component

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,6 +4,12 @@ import { getAuth,
     signInWithPopup, 
     GoogleAuthProvider 
 } from 'firebase/auth';
+import { getFirestore, 
+    doc, 
+    getDoc, 
+    setDoc,
+} from 'firebase/firestore';
+
 
 
 
@@ -26,3 +32,40 @@ const firebaseConfig = {
 
   export const auth = getAuth();
   export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+  export const db = getFirestore();
+  
+  export const createUserDocumentFromAuth = async (userAuth) => {
+    // doc() takes three arguments. Database, collection, and an identifier(can use the uid from google sign in)
+        const userDocRef = doc(db, 'users', userAuth.uid);
+
+        const userSnapshot = await getDoc(userDocRef);
+        console.log(userSnapshot)
+        console.log(userSnapshot.exists())
+        //esixts() helps to show if a document exist in the collection
+
+        if(!userSnapshot.exists()) {
+            const {displayName, email} = userAuth;
+            const createdAt = new Date();
+
+            try {
+                await setDoc(userDocRef, {
+                    displayName, 
+                    email, 
+                    createdAt
+                });
+                } catch (error) {
+                    console.log('error creating the user', error.message);
+            }
+        }
+
+        //check if user data exists
+
+        //if user data doesn't exists create/ set the document with the data from 
+        //userAuth in my collection.
+
+
+        //if user data exists
+        return userDocRef;
+
+  };
