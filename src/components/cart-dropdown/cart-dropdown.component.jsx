400e2015a3b57cad15b0c42fe0ebdcbdc7c2281f
RDixonCodes <rhdixon79@gmail.com>
2023-04-13 22:09:16 -0700

    cart dropdown converted to style component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,11 +2,11 @@ import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { CartContext } from '../../contexts/cart.context';
+import { CartDropdownContainer, CartItems, EmptyMessage } from './cart-dropdown.styles';
 
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component'
 
-import './cart-dropdown.styles.scss'
 
 const CartDropdwon = () => {
 
@@ -17,12 +17,15 @@ const CartDropdwon = () => {
         navigate('/checkout')
     }
     return(
-        <div className='cart-dropdown-container'>
-            <div className='cart-items'>
-                {cartItems.map((item) => <CartItem cartItem={item} key={item.id}/>)}
-            </div>
+        <CartDropdownContainer>
+            <CartItems>
+                {cartItems.length ? 
+                cartItems.map((item) => <CartItem cartItem={item} key={item.id}/>) 
+                : ( <EmptyMessage>Your cart is empty </EmptyMessage> )
+                }
+            </CartItems>
             <Button onClick={goToCheckoutHandler}>go to checkout</Button>
-        </div>
+        </CartDropdownContainer>
     )
 }
 
