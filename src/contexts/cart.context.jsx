cefdeaa77e65abc2d6664d780a220aad9f715712
RDixonCodes <rhdixon79@gmail.com>
2023-04-20 23:03:49 -0700

    reducer added to utils/update to cart & user context

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,6 @@
-import { createContext, useEffect, useState, useReducer } from "react";
+import { createContext, useReducer } from "react";
+
+import { createAction } from '../utils/reducer/reducer.utils';
 
 const addCartItem = (cartItems, productToAdd) => {
     // find if cartItems contains productToAdd
@@ -96,21 +98,12 @@ export const CartProvider = ({children}) => {
             0
         );
 
-        dispatch({ type: CART_ACTIONS_TYPES.SET_CART_ITEMS, 
-            payload: { 
+        dispatch(
+            createAction(CART_ACTIONS_TYPES.SET_CART_ITEMS,{ 
                 cartItems: newCartItems, 
                 cartTotal: newCartTotal, 
-                cartCount: newCartCount}
-            });
-/**generate newCartTotal
- * 
- * generate newCartCount 
- * 
- * dispatch new action with payload = {
- * newCartItem,
- * newCartTotal,
- * newCartCount,
- * }*/
+                cartCount: newCartCount})
+        );
     }
 
 
@@ -131,7 +124,7 @@ export const CartProvider = ({children}) => {
 
     const setIsCartOpen =(bool) => {
 
-        dispatch({ type: CART_ACTIONS_TYPES.SET_IS_CART_OPEN, payload: bool });
+        dispatch(createAction( CART_ACTIONS_TYPES.SET_IS_CART_OPEN, bool ));
     }
 
     const value = { isCartOpen, 
--- src/contexts/user.contexts.jsx
+++ src/contexts/user.contexts.jsx
@@ -2,6 +2,7 @@ import { createContext, useEffect, useReducer } from "react";
 
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from '../utils/firebase/firebase.utils'
 
+import { createAction } from "../utils/reducer/reducer.utils";
 
 //this is the actual value you want to access
 export const UserContext = createContext({
@@ -39,7 +40,7 @@ export const UserProvider = ({ children }) => {
     console.log(currentUser)
 
     const setCurrentUser = (user) => {
-        dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user })
+        dispatch(createAction( USER_ACTION_TYPES.SET_CURRENT_USER, user ))
     }
 
 
