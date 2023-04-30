c24ffd290aae2f40315b9605917512cf51b3ac1f
RDixonCodes <rhdixon79@gmail.com>
2023-04-30 15:02:16 -0700

    added persist to app

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
 
 import { rootReducer } from './root-reducer';
 
@@ -17,5 +18,15 @@ const loggerMiddleware = (store) => (next) => (action) => {
     console.log('next state', store.getState())
 }
 
-export const store = configureStore({reducer: rootReducer, middleware: [loggerMiddleware]})
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({reducer: persistedReducer, middleware: [loggerMiddleware]})
+
+export const persistor = persistStore(store);
 
